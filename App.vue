c13f6631ad5095a432b485dbbb2314fa2fa2488d
mariagrs <43874158+mariagrs@users.noreply.github.com>
2020-04-09 14:35:27 +0200

    Food Card et 2nd navbar ajouté

---
+++ App.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-app>
+  <v-content>
+    <router-view></router-view>
+  </v-content>
+  </v-app>
+</template>
+
+<script>
+// import Home from './views/Home.vue'
+
+export default {
+  name: 'App',
+  data: () => ({
+  }),
+  methods: {
+    Search () {
+      alert('hello')
+    }
+  }
+}
+</script>
+
+<style>
+  .search-bar{
+    margin-top: 30px;
+    margin-left: 380px;
+    width: 600px;
+  }
+</style>
---
+++ FoodCard.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-card class="d-inline-block mx-auto" flat>
+    <v-container>
+      <v-row dense>
+        <v-col
+          v-for="card in cards"
+          :key="card.title"
+          :cols="card.flex"
+        >
+          <v-card>
+            <v-img
+              :src="card.src"
+              class="white--text align-end"
+              gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+              height="200px"
+            >
+              <v-card-title v-text="card.title"></v-card-title>
+            </v-img>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-card>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    cards: [
+      { title: 'Pre-fab homes', src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg', flex: 3 },
+      { title: 'Favorite road trips', src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg', flex: 3 },
+      { title: 'Best airlines', src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg', flex: 3 },
+      { title: 'Favorite road trips', src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg', flex: 3 }
+    ]
+  })
+}
+</script>
