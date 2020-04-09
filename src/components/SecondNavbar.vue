17456cc81823065525970bbbe83b3ccd5d45e84d
Maria Graus <maria.graus1998@gmail.com>
2020-04-09 13:50:46 +0200

    FoodCardAdd

---
+++ src/components/SecondNavbar.vue
@@ -0,0 +1,180 @@
+<template>
+  <div>
+  <v-toolbar
+    color="#e3e3e3"
+    text
+    flat
+    dense
+  >
+    <div class="mx-12"></div>
+    <div class="mx-9"></div>
+
+    <v-menu offset-y open-on-hover>
+      <template v-slot:activator="{ on }">
+        <v-btn
+          text
+          v-on="on"
+        >
+          Recipes
+        </v-btn>
+      </template>
+      <v-list>
+        <v-list-item
+          v-for="item in links"
+          :key="item.title"
+        >
+          <v-list-item-title>{{ item.title }}</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
+
+    <v-divider
+      class="mx-12"
+      inset
+      vertical
+    ></v-divider>
+
+    <v-menu offset-y open-on-hover>
+      <template v-slot:activator="{ on }">
+        <v-btn
+          text
+          v-on="on"
+        >
+          Healthy Recipes
+        </v-btn>
+      </template>
+      <v-list>
+        <v-list-item
+          v-for="item in links"
+          :key="item.title"
+        >
+          <v-list-item-title>{{ item.title }}</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
+
+    <v-divider
+      class="mx-12"
+      inset
+      vertical
+    ></v-divider>
+
+    <v-menu offset-y open-on-hover>
+      <template v-slot:activator="{ on }">
+        <v-btn
+          text
+          v-on="on"
+        >
+          Theme
+        </v-btn>
+      </template>
+      <v-list>
+        <v-list-item
+          v-for="item in links"
+          :key="item.title"
+        >
+          <v-list-item-title>{{ item.title }}</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
+
+    <v-divider
+      class="mx-12"
+      inset
+      vertical
+    ></v-divider>
+
+    <v-menu offset-y open-on-hover>
+      <template v-slot:activator="{ on }">
+        <v-btn
+          text
+          v-on="on"
+        >
+          Product
+        </v-btn>
+      </template>
+      <v-list>
+        <v-list-item
+          v-for="item in links"
+          :key="item.title"
+        >
+          <v-list-item-title>{{ item.title }}</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
+
+    <v-divider
+      class="mx-12"
+      inset
+      vertical
+    ></v-divider>
+
+    <v-menu offset-y open-on-hover>
+      <template v-slot:activator="{ on }">
+        <v-btn
+          text
+          v-on="on"
+        >
+          Children
+        </v-btn>
+      </template>
+      <v-list>
+        <v-list-item
+          v-for="item in links"
+          :key="item.title"
+        >
+          <v-list-item-title>{{ item.title }}</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
+
+    <v-divider
+      class="mx-12"
+      inset
+      vertical
+    ></v-divider>
+
+    <v-menu offset-y open-on-hover>
+      <template v-slot:activator="{ on }">
+        <v-btn
+          text
+          v-on="on"
+        >
+          Cocktails
+        </v-btn>
+      </template>
+      <v-list>
+        <v-list-item
+          v-for="item in links"
+          :key="item.title"
+        >
+          <v-list-item-title>{{ item.title }}</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
+
+  </v-toolbar>
+  </div>
+</template>
+
+<script>
+
+export default {
+
+  name: 'SecondNavbar',
+  drawer: false,
+  data () {
+    return {
+      links: [
+        { title: 'Recipes' },
+        { title: 'Main Dish' },
+        { title: 'Dessert' }
+      ]
+    }
+  }
+
+}
+</script>
+
+<style>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,110 @@
 <template>
-  <div class="home">
-    <HelloWorld/>
-  </div>
+  <v-app>
+    <v-app-bar app
+      color="#e3e3e3"
+    >
+      <v-img
+        contain
+        class="shrink mt-1"
+        :src="require('../assets/logocuisine.png')"
+        height="65"
+        width="70"
+        position="left"
+      />
+      <v-toolbar-title
+        class="headline"
+      >
+      What can I cook
+      </v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <div class="search-bar">
+      <v-row>
+      <v-col cols="12">
+          <v-text-field
+            solo
+            label="Ingredient, dish, keyword.."
+            append-icon='fa-search'
+            @click:append="Search"
+          >
+          </v-text-field>
+
+        </v-col>
+      </v-row>
+      </div>
+
+    </v-app-bar>
+    <SecondNavbar/>
+
+    <v-container
+    height="500"
+    width="100%"
+    >
+    <v-card
+    outlined
+    >
+      <v-row>
+        <v-col cols="6"
+        sm="6">
+          <br><br><br>
+          <h2 class=" mt-12 display-3 text-center">Try this new recipe !</h2>
+          <p class=" mt-8 headline text-center">Chinese Noodles with mushrooms</p>
+        </v-col>
+        <v-col cols="6"
+        sm="6">
+        <v-img
+          :src="require('../assets/chinese-food.jpg')"
+          height="100%"
+          width="95%"
+          class="rounded"
+        />
+        </v-col>
+      </v-row>
+    </v-card>
+    </v-container>
+    <br><br>
+    <v-card class="d-inline-block mx-auto" flat>
+    <h3 class="grey--text">En manque d'inspiration ?(anglais)</h3>
+    </v-card>
+    <FoodCard/>
+    <br>
+    <FoodCard/>
+    <br>
+    <FoodCard/>
+    <!--<p>hello</p>-->
+  </v-app>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import SecondNavbar from '@/components/SecondNavbar.vue'
+import FoodCard from '@/components/FoodCard.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    SecondNavbar,
+    FoodCard
+  },
+  data: () => ({
+    //
+  }),
+  methods: {
+    Search () {
+      alert('hello')
+    }
   }
 }
 </script>
+
+<style>
+  .search-bar{
+    margin-top: 30px;
+    margin-left: 380px;
+    width: 600px;
+  }
+  .rounded{
+    border-radius: 5%;
+  }
+</style>
