f5903d57a31c9123a164ced0e33c6fbb5043201a
Anil9757 <56797977+Anil9757@users.noreply.github.com>
2020-04-17 23:18:36 +0200

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Recipe from '../views/RecipeInstruction.vue'
+import Cocktail from '../views/Cocktail.vue'
 
 Vue.use(VueRouter)
 
@@ -10,6 +11,11 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/Cocktail/:id',
+    name: 'Cocktail',
+    component: Cocktail
   },
   {
     path: '/RecipeInstruction',
