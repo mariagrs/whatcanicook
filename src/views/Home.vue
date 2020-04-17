1cef91f6c0e8361bd08a33d436dd729ffcb68e38
mariagrs <43874158+mariagrs@users.noreply.github.com>
2020-04-17 11:01:02 +0200

    add views

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,116 @@
 <template>
-  <div class="home">
-    <HelloWorld/>
-  </div>
+  <v-app>
+    <v-container
+    height="500"
+    width="100%"
+    >
+    <v-card
+    outlined
+    >
+      <v-row
+        v-for="result2 in results2"
+        :key="result2.id"
+        :to ="`/Recipe/${result2.id}`">
+        <v-col cols="6"
+        sm="6">
+          <br><br><br>
+          <h2 class=" mt-12 display-3 text-center">Try this recipe !</h2>
+          <p class=" mt-8 headline text-center">{{ result2.title }}</p>
+        </v-col>
+        <v-col cols="6"
+        sm="6">
+        <v-img
+          @click="$router.push(`/Recipe/${result2.id}`)"
+          :src="imageSrc(result2.id)"
+          height="100%"
+          width="95%"
+          class="rounded"
+        />
+        </v-col>
+      </v-row>
+    </v-card>
+    </v-container>
+    <br><br>
+    <v-card class="d-inline-block mx-auto" flat>
+    <h2 class="black--text">Lack of inspiration ?</h2>
+    </v-card>
+
+    <v-card class="d-inline-block mx-auto" flat>
+      <v-container>
+      <v-row>
+        <v-col
+        v-for="result in results"
+        :key="result.id"
+        cols="3"
+        >
+        <FoodCard :src="imageSrc(result.id)" :title="result.title"/>
+        </v-col>
+      </v-row>
+      </v-container>
+    </v-card>
+
+    <div class="text-right">
+      <v-btn
+        fab
+        dark
+        color="black"
+        @click="toTop"
+        class="mx-12 white--text"
+      >
+      <v-icon dark>mdi-arrow-up</v-icon>
+      </v-btn>
+    </div>
+  </v-app>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import FoodCard from '@/components/FoodCard.vue'
+import axios from 'axios'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    FoodCard
+  },
+  data: () => ({
+    results: [],
+    results2: []
+  }),
+  methods: {
+    toTop () {
+      this.$vuetify.goTo(0)
+    },
+    imageSrc (id) {
+      return 'https://spoonacular.com/recipeImages/' + id + '-240x150.jpg'
+    }
+  },
+  mounted () {
+    var params = {
+      apiKey: 'a580fafc28554f4a9ac047dcd8325266',
+      number: 12
+    }
+    var params2 = {
+      number: 1
+    }
+    axios.get('https://api.spoonacular.com/recipes/random', { params }).then(res => {
+      this.results = res.data.recipes
+      console.log(res.data)
+    })
+    axios.get('https://api.spoonacular.com/recipes/random?apiKey=a580fafc28554f4a9ac047dcd8325266', { params2 }).then(res1 => {
+      this.results2 = res1.data.recipes
+      console.log(res1.data.recipes)
+    })
   }
 }
 </script>
+
+<style>
+  .search-bar{
+    margin-top: 30px;
+    margin-left: 380px;
+    width: 600px;
+  }
+  .rounded{
+    border-radius: 5%;
+  }
+</style>
---
+++ src/views/Recipe.vue
@@ -0,0 +1,147 @@
+<template>
+  <v-app>
+    <v-container
+    height="auto"
+    >
+      <v-card
+      color="black"
+      height="auto">
+      <v-card-title class="justify-center title white--text">{{results.title}}</v-card-title>
+      </v-card>
+      <v-card outlined>
+        <v-row align="center">
+          <v-col cols="5">
+          <v-img
+            :src="imageSrc(results.id)"
+            height="auto"
+            width="100%"
+            class="rounded ml-2"
+          />
+          </v-col>
+          <v-col cols="7">
+              <h3>Realized by <i class="body-1">{{ results.sourceName}} </i></h3>
+              <h4 class="subtitle-1"  v-html="results.summary">
+              </h4>
+          </v-col>
+        </v-row>
+      </v-card>
+      <v-card
+      color="black"
+      height="40">
+          <v-row align="center" justify="center">
+          <v-tooltip bottom>
+          <template v-slot:activator="{ on }">
+          <v-icon color="white" class="mt-2 mr-2" v-on="on">fas fa-user</v-icon>
+          </template>
+          <span>servings</span>
+          </v-tooltip>
+          <h4 class=" mt-2 white--text">{{results.servings}}</h4>
+
+          <v-tooltip bottom>
+          <template v-slot:activator="{ on }">
+          <v-icon color="white" class="mt-2 ml-12 mr-2" v-on="on">far fa-clock</v-icon>
+          </template>
+          <span>time</span>
+          </v-tooltip>
+          <h4 class=" mt-2 white--text">{{results.readyInMinutes}}</h4>
+
+          <v-tooltip bottom>
+          <template v-slot:activator="{ on }">
+          <v-icon color="white" class="mt-2 ml-12 mr-2" v-on="on">fas fa-seedling</v-icon>
+          </template>
+          <span>vegan</span>
+          </v-tooltip>
+          <h4 class=" mt-2 white--text">{{results.vegan}}</h4>
+
+          <v-tooltip bottom>
+          <template v-slot:activator="{ on }">
+          <v-icon color="white" class="mt-2 ml-12 mr-2" v-on="on">mdi-barley</v-icon>
+          </template>
+          <span>gluten-free</span>
+          </v-tooltip>
+          <h4 class=" mt-2 white--text">{{results.glutenFree}}</h4>
+          </v-row>
+      </v-card>
+      <v-card
+        outlined
+        height="auto">
+       <h3 class="ml-4 mt-5">
+          Ingredients :
+        </h3>
+        <v-row v-for="result in results.extendedIngredients"
+          :key="result.id">
+          <v-col>
+            <v-avatar
+            class="rounded ml-6"
+            size="80"
+            tile
+            >
+            <v-img :src="imageIngredient(result.image)"></v-img>
+            </v-avatar>
+          </v-col>
+          <v-col cols="10">
+            <p class="font-weight-medium mx-auto">{{result.originalString}}</p>
+          </v-col>
+        </v-row>
+        <h3 class="ml-4 mt-5">Directions :</h3>
+        <p class="mx-5" v-html="results.instructions"></p>
+      </v-card>
+    </v-container>
+    <div class="text-right">
+    <v-btn
+        fab
+        dark
+        color="black"
+        @click="toTop"
+        class="mx-12 white--text"
+      >
+      <v-icon dark>mdi-arrow-up</v-icon>
+    </v-btn>
+    </div>
+  </v-app>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'Recipe',
+  data: () => ({
+    results: []
+  }),
+  methods: {
+    getId () {
+      return this.$route.params.id
+    },
+    imageSrc (id) {
+      return 'https://spoonacular.com/recipeImages/' + id + '-240x150.jpg'
+    },
+    imageIngredient (image) {
+      return ' https://spoonacular.com/cdn/ingredients_100x100/' + image
+    },
+    toTop () {
+      this.$vuetify.goTo(0)
+    }
+  },
+  mounted () {
+    var params = {
+      apiKey: 'a580fafc28554f4a9ac047dcd8325266'
+    }
+    axios.get('https://api.spoonacular.com/recipes/' + this.getId() + '/information/', { params }).then(res => {
+      this.results = res.data
+      console.log(this.results)
+    })
+  }
+}
+</script>
+
+<style>
+  .search-bar{
+    margin-top: 30px;
+    margin-left: 380px;
+    width: 600px;
+  }
+  .rounded{
+    border-radius: 50%;
+  }
+</style>
