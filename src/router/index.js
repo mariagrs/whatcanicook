4fcd312ecd5b9c511f4cce1cac1d9287d1b05287
mariagrs <43874158+mariagrs@users.noreply.github.com>
2020-04-17 10:56:29 +0200

    update router

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Recipe from '../views/Recipe.vue'
+import Category from '../views/Category.vue'
+import Cocktail from '../views/Cocktail.vue'
 
 Vue.use(VueRouter)
 
@@ -11,13 +14,21 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/Recipe/:id',
+    name: 'Recipe',
+    component: Recipe
+  },
+  {
+    path: '/Cocktail/:id',
+    name: 'Cocktail',
+    component: Cocktail
+  },
+  {
+    path: '/Category/:type/:name',
+    name: 'Category',
+    component: Category
   }
+
 ]
 
 const router = new VueRouter({
