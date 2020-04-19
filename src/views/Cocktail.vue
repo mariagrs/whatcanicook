67d7d05d49d8d8028ffcf00e3596d11ba6eb5a24
Anil9757 <56797977+Anil9757@users.noreply.github.com>
2020-04-19 23:56:51 +0200

    Update Cocktail.vue

--- src/views/Cocktail.vue
+++ src/views/Cocktail.vue
@@ -3,94 +3,62 @@
     <v-container
     height="auto"
     >
-    <v-card flat v-for="result in results.drinks" :key="result.id">
-        <v-card-title class="display-1 justify-center">{{result.strDrink}}</v-card-title>
-        <v-col align="center" justify="center">
-        </v-col>
-    </v-card>
-    <div class="image" v-for="result in results.drinks" :key="result.id">
-         <v-row align="center">
-          <v-col cols="5">
-          <center><v-img
-            :src="result.strDrinkThumb"
-            height="auto"
+      <v-card
+      color="black"
+      height="auto"
+      v-for="result in alcoholicDrinks"
+      :key="result.idDrink">
+      <v-card-title class="justify-center title white--text">{{result.strDrink}}</v-card-title>
+      </v-card>
+      <v-card outlined>
+        <v-row align="center"
+        v-for="result in alcoholicDrinks"
+        :key="result.idDrink">
+          <v-col cols="6">
+          <v-img
+            :src="result.strDrinkThumb + '/preview'"
+            height="500"
             width="100%"
             class="rounded ml-2"
-            position="center"
-          /></center>
-          </v-col>
-          <v-col cols="7">
-              <h3>Glass Type : {{result.strGlass}}</h3>
-              <br>
-              <h3>Type Beverage : {{result.strAlcoholic}}</h3>
-              <h4 class="subtitle-1">
-              </h4>
-          </v-col>
-        </v-row>
-      </div>
-    <v-card
-    color="black"
-    height="40">
-        <v-row align="center" justify="center">
-        <v-tooltip bottom>
-        <template v-slot:activator="{ on }">
-        <v-icon color="white" class="mt-2 mr-5" v-on="on">fas fa-user</v-icon>
-        </template>
-        <span>servings</span>
-        </v-tooltip>
-        <h4 class=" mt-2 white--text">6</h4>
-
-        <v-tooltip bottom>
-        <template v-slot:activator="{ on }">
-        <v-icon color="white" class="mt-2 ml-12 mr-5" v-on="on">far fa-clock</v-icon>
-        </template>
-        <span>prep time</span>
-        </v-tooltip>
-        <h4 class=" mt-2 white--text">35 min</h4>
-
-        <v-tooltip bottom>
-        <template v-slot:activator="{ on }">
-        <v-icon color="white" class="mt-2 ml-12 mr-5" v-on="on">fas fa-dollar-sign</v-icon>
-        </template>
-        <span>cost</span>
-        </v-tooltip>
-        <h4 class=" mt-2 white--text">$$$</h4>
-        </v-row>
-    </v-card>
-     <v-card
-      flat
-      outlined
-      height="auto"
-      justify="center">
-        <v-card-title>Ingredients :</v-card-title>
-        <v-row class="text-wrap ml-4" v-for="result in results.drinks" :key="result.id">
-          <v-row class="text-wrap ml-4" v-for="n in 10" :key="n">
-            <v-col v-if="result['strIngredient'+ n] != null">
-                           <v-avatar
-            class="rounded ml-6"
-            size="80"
-            tile
-            >
-              <v-img
-            :src="getIngredientImage(result['strIngredient' + n])"
           />
-            </v-avatar>
-                        <v-col cols="11" v-if="result['strIngredient' + n] != null">
-            <p class="font-weight-medium mx-auto"><b >Name: {{result['strIngredient' + n]}}</b>- Measure: {{result['strMeasure' + n]}} </p>
-            </v-col>
-
           </v-col>
-          </v-row>
-
-            </v-row>
-            <v-card-title>Instruction :</v-card-title>
-            <v-row v-for="result in results.drinks" :key="result.id">
-              <v-col>
-          <p class="text-wrap ml-4"><b>{{result.strInstructions}}</b></p>
-              </v-col>
-            </v-row>
-    </v-card>
-   <div class="text-right">
+          <v-col cols="6">
+              <h3 class="text-center">Glass type : <i class="body-1">{{ result.strGlass}} </i></h3>
+          </v-col>
+        </v-row>
+      </v-card>
+      <v-card
+      color="black"
+      height="40">
+      </v-card>
+      <v-card
+        outlined
+        height="auto"
+        v-for="result in alcoholicDrinks"
+        :key="result.idDrink">
+       <h3 class="ml-4 mt-5">
+          Ingredients :
+        </h3>
+        <v-row
+        v-for="index in 15"
+        :key="index">
+          <v-col v-if="result['strIngredient' + index] != null">
+            <v-avatar
+            class="rounded ml-6 mb-5"
+            size="80"
+            tile>
+            <v-img :src="imageIngredient(result['strIngredient' + index])"></v-img>
+            </v-avatar>
+          </v-col>
+          <v-col cols="10" v-if="result['strIngredient' + index] != null">
+            <p class="font-weight-medium mx-auto"> {{result['strMeasure' + index]}} {{result['strIngredient' + index]}}</p>
+          </v-col>
+        </v-row>
+        <h3 class="ml-4 mt-5">Directions :</h3>
+        <p class="mx-5" v-text="result.strInstructions"></p>
+      </v-card>
+    </v-container>
+    <div class="text-right">
     <v-btn
         fab
         dark
@@ -101,66 +69,36 @@
       <v-icon dark>mdi-arrow-up</v-icon>
     </v-btn>
     </div>
-    </v-container>
-
   </v-app>
 </template>
 
 <script>
 import axios from 'axios'
-
 export default {
   name: 'Cocktail',
   data: () => ({
-    results: []
+    alcoholicDrinks: []
   }),
   methods: {
-
     getId () {
       return this.$route.params.id
     },
-
-    getNumber (results, number) {
-      return results.strIngredient + number
-      // eslint-disable-next-line no-unreachable
-      console.log('result.strDrink' + number)
+    imageIngredient (name) {
+      return ' https://www.thecocktaildb.com/images/ingredients/' + name + '-Small.png'
     },
-
-    getIngredientImage (image) {
-      return 'https://www.thecocktaildb.com/images/ingredients/' + image + '.png'
-    },
-
     toTop () {
       this.$vuetify.goTo(0)
     }
   },
   mounted () {
     axios.get('https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=' + this.getId()).then(res => {
-      this.results = res.data
-      console.log(this.results)
+      this.alcoholicDrinks = res.data.drinks
     })
   }
 }
 </script>
-
 <style>
-  .search-bar{
-    margin-top: 30px;
-    margin-left: 380px;
-    width: 600px;
-  }
   .rounded{
     border-radius: 5%;
   }
-
-  .ingredients{
-    position: relative;
-    left: 10px;
-  }
-
-  .position{
-    position: relative;
-    left: 40px;
-  }
-
 </style>
